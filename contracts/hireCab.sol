6d3ca627e2102011c6549903864f0996aa84efc2
Anas748 <2021387@student.cct.ie>
2023-11-28 20:21:52 +0000

    Initialize the TaxiRentalSystem contract. mapping for profiles. events to store the values

--- contracts/hireCab.sol
+++ contracts/hireCab.sol
@@ -1,6 +1,37 @@
 // SPDX-License-Identifier: GPD-3.0
 pragma solidity >0.4.0 <0.9.0;
 
+contract TaxiRentalSystem {
+    struct UserProfile {
+        string name;
+        uint8 rating;
+    }
+
+
+    mapping(address => UserProfile) public passengerProfiles;
+    mapping(address => UserProfile) public driverProfiles;
+
+
+    address public passenger;
+    address public driver;
+    string public pickupLocation;
+    string public dropoffLocation;
+    uint256 public distanceKM;
+    uint256 public fare;
+    bool public fareOffered;
+    bool public fareAccepted;
+    bool public destinationArrived;
+    bool public farePaid;
+    uint8 public passengerRating;
+   
+
+
+    event FareOffered(uint256 fare);
+    event FareAccepted();
+    event DestinationArrived();
+    event FarePaid();
+    event PassengerRated(uint8 rating);
+    
 
 
 
