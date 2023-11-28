e44d0909be12df63842218727d315994638a76dd
Anas748 <2021387@student.cct.ie>
2023-11-28 20:38:19 +0000

    modifiers for validation

--- contracts/hireCab.sol
+++ contracts/hireCab.sol
@@ -48,5 +48,46 @@ contract TaxiRentalSystem {
         passengerRating = 0;
     }
 
+      // Modifier: Ensures that only the passenger can call the function
+    modifier onlyPassenger() {
+        require(msg.sender == passenger, "Only the passenger can call this function");
+        _;
+    }
 
+    // Modifier: Ensures that only the driver can call the function
+    modifier onlyDriver() {
+        require(msg.sender == driver, "Only the driver can call this function");
+        _;
+    }
+
+    // Modifier: Ensures that fare has not been offered yet
+    modifier notYetOffered() {
+        require(!fareOffered, "Fare has already been offered");
+        _;
+    }
+
+    // Modifier: Ensures that fare has not been accepted yet
+    modifier notYetAccepted() {
+        require(!fareAccepted, "Fare has already been accepted");
+        _;
+    }
+
+    // Modifier: Ensures that destination has not arrived yet
+    modifier notYetArrived() {
+        require(!destinationArrived, "Destination has already arrived");
+        _;
+    }
+
+    // Modifier: Ensures that fare has not been paid yet
+    modifier notYetPaid() {
+        require(!farePaid, "Fare has already been paid");
+        _;
+    }
+
+    // Modifier: Ensures that the passenger can rate only after the destination is arrived and fare is paid
+    modifier canRate() {
+        require(destinationArrived && farePaid, "You can only rate after the destination is arrived and fare is paid");
+        require(passengerRating == 0, "You have already rated the driver");
+        _;
+    }
 
